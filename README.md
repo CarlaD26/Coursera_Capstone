b405261f399bc8be793717972de4871ce3bfce4b
Carla Deaconu <carladeaconu@Carlas-MacBook-Air.local>
2020-08-03 15:54:07 +0100

    text added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Coursera_Capstone
 This repository is for the Capstone assignments
+
+This notebook will be mainly used for the capstone project.
--- Scripts/Jupyter_Notebook_Capstone.py
+++ Scripts/Jupyter_Notebook_Capstone.py
@@ -1,3 +1,4 @@
 import pandas as pd
 import numpy as np
 print("Hello Capstone Project Course!")
+This notebook will be mainly used for the capstone project.
